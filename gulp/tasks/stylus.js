6c1b707401b8463abe580b0b7b8b2774d2bbfec9
Apox <xzzy159@gmail.com>
2014-04-11 18:50:43 +0200

    Change bootstrap

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -1,13 +1,12 @@
 var gulp       = require('gulp');
 var stylus = require('gulp-stylus');
 var nib = require('nib');
-var boostrap = require('bootstrap-stylus');
 var livereload = require('gulp-livereload');
 var notify     = require('gulp-notify');
 
 module.exports = function() {
     return gulp.src('./src/stylus/*.stylus')
-        .pipe(stylus({use: [nib(),boostrap()]}))
+        .pipe(stylus({use: [nib()]}))
         .pipe(gulp.dest('./build'))
         .on('error', notify.onError({
             message: "<%= error.message %>",
